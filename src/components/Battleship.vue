dbb2c4571d30c4fd7d6201a281a9aa41be173378
Andrea Mogren <andrea.mogren91@gmail.com>
2019-05-21 21:49:10 +0200

    worked with mimmi on adding more ships :)

--- src/components/Battleship.vue
+++ src/components/Battleship.vue
@@ -6,7 +6,6 @@
       <td v-for="(cell, cellIndex) in 7" :id="rowIndex + '-' + cellIndex" @click="checkHit"> <!-- :class="{ hit: hit, miss: miss }" -->
       </td>
       </tr>
-
     </table>
     <h3>0 1 2 3 4 5 6</h3>
   </div>
@@ -19,9 +18,19 @@
     return {
       rows: 7,
       squares: 7,
-      /*randomRow: String,
+      randomRow: String,
       randomCell: String,
-      singleShip: String,
+      randomRow2: String,
+      randomCell2: String,
+      randomRow3: String,
+      randomCell3: String,
+      randomRow4: String,
+      randomCell4: String,
+      randomRow5: String,
+      randomCell5: String,
+      randomRow6: String,
+      randomCell6: String,
+      /*singleShip: String,
       Destoryer: String, //2
       Destoryer2: String, //2
       Cruiser: String, //3
@@ -50,44 +59,82 @@
       //https://codepen.io/CodifyAcademy/pen/ByBEOz
       console.log(event.target.id);
       /* Hur göra detta nu? */
-      if (event.target.id !== this.randomPosition){
+      if (event.target.id !== this.singleShip){
         this.hit = false;
         this.miss = true;
         console.log(this.hit);
       }
-      if (event.target.id === this.randomPosition){
+      if (event.target.id === this.singleShip){//this.randomPosition){
           this.hit = true;
           this.miss = false;
           console.log(this.hit);
           alert("nooo me ship!! >:(");
         }
       },
+
+      //Anteckningar:
+      //Börja med ett skepp, loopa igenom. Om dens position är upptagen, ta en annan.
+      //en if för lodrätt, en else för vågrätt
+      //Räcker med att skeppet kan gå åt höger eller neråt
+
+      /* Exempel på hur man kan lägga till längd på skepp
+      Horisontell:
+      for (int i = 0; i < ship.Length; i++)
+          {
+              ship.Coordinates.Add(new ShipCoordinate()
+              {
+                  Coordinate = letter.ToString() + randomNumber.ToString()
+              });
+              letter++;
+          }
+          shipIsNotPlaced = false;
+      }
+      Vertikal: 
+      for (int i = 0; i < ship.Length; i++)
+                        {
+                            ship.Coordinates.Add(new ShipCoordinate()
+                            {
+                                Coordinate = letter.ToString() + randomNumber.ToString()
+                            });
+                            randomNumber++;
+                        }
+      */
+
+
+
       /* How to not make these numbers overlap */
         singleShip: function() {
           this.randomRow = Math.floor(Math.random() * (this.max - this.min) + this.min);
           this.randomCell = Math.floor(Math.random() * (this.max - this.min) + this.min);
           this.singleShip = this.randomRow + "-" + this.randomCell;
           console.log("Single ship", this.singleShip);
+          console.log("randomRow: ", this.randomRow);
         },
         Destroyer: function (){
-          this.randomRow = Math.floor(Math.random() * (this.max - this.min) + this.min);
-          this.randomCell = Math.floor(Math.random() * (this.max - this.min) + this.min);
-          this.Destroyer = this.randomRow + "-" + this.randomCell;
-          this.Destroyer2 = this.randomRow + "-" + (this.randomCell + 1);
-          console.log("Destroyer ", this.Destroyer, this.Destroyer2);
-          console.log("Randoms destroyer ", this.randomRow, this.randomCell);
+          this.randomRow2 = Math.floor(Math.random() * (this.max - this.min) + this.min);
+          this.randomCell2 = Math.floor(Math.random() * (this.max - this.min) + this.min);
+          console.log("randomRow2: ", this.randomRow2);
+          while (this.randomRow2 === this.randomRow) { //sålänge det finns en konflikt mellan id:na, gör ett nytt randomRow2 nummer
+              console.log("konflikt");
+              this.randomRow2 = Math.floor(Math.random() * (this.max - this.min) + this.min);
+          }
+            console.log("new value randomRow2: ", this.randomRow2);
+            this.Destroyer = this.randomRow2 + "-" + this.randomCell2;
+            this.Destroyer2 = this.randomRow2 + "-" + (this.randomCell2 + 1);
+            console.log("Destroyer ", this.Destroyer, this.Destroyer2);
+            console.log("Randoms destroyer ", this.randomRow, this.randomCell);
         },
         Cruiser: function(){
-          this.randomRow = Math.floor(Math.random() * (this.max - this.min) + this.min);
-          this.randomCell = Math.floor(Math.random() * (this.max - this.min) + this.min);
+          this.randomRow3 = Math.floor(Math.random() * (this.max - this.min) + this.min);
+          this.randomCell3 = Math.floor(Math.random() * (this.max - this.min) + this.min);
           this.Cruiser = this.randomRow + "-" + this.randomCell;
           this.Cruiser2 = this.randomRow + "-" + (this.randomCell + 1);
           this.Cruiser3 = this.randomRow + "-" + (this.randomCell + 2);
           console.log("Cruiser", this.Cruiser, this.Cruiser2, this.Cruiser3);
         },
         Submarine: function() {
-          this.randomRow = Math.floor(Math.random() * (this.max - this.min) + this.min);
-          this.randomCell = Math.floor(Math.random() * (this.max - this.min) + this.min);
+          this.randomRow4 = Math.floor(Math.random() * (this.max - this.min) + this.min);
+          this.randomCell4 = Math.floor(Math.random() * (this.max - this.min) + this.min);
           this.Submarine = this.randomRow + "-" + this.randomCell;
           this.Submarine2 = this.randomRow + "-" + (this.randomCell + 2);
           this.Submarine3 = this.randomRow + "-" + (this.randomCell + 3);
